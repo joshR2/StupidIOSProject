cecf746211b9d7070b1158061c6e58520c56b59c
joshR2 <josh2cool365@aol.com>
2015-12-11 09:54:57 -0700

    First IOS Project/App

---
+++ StupidIOSProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StupidIOSProject
+//
+//  Created by Roberts, Joshua on 12/11/15.
+//  Copyright (c) 2015 Roberts, Joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
